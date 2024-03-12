0c0292c518e17963cbe8cf5a91b1674d6097458e
atran14 <atran14@uw.edu>
2024-03-12 15:40:46 -0700

    First Commit

---
+++ src/main/java/org/akiratran/hibermusic/HiberMusicApplication.java
@@ -0,0 +1,13 @@
+package org.akiratran.hibermusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiberMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/akiratran/hibermusic/HiberMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akiratran.hibermusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
