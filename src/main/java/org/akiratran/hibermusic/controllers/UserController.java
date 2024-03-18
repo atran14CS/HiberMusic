a749ac62b2e1d62b5d535ef8874c8e4b73f4f031
atran14 <atran14@uw.edu>
2024-03-18 09:19:28 -0700

    More notes

--- src/main/java/org/akiratran/hibermusic/controllers/UserController.java
+++ src/main/java/org/akiratran/hibermusic/controllers/UserController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Controls the different url endpoints, each endpoint returns a different view
+ * depending on the logic.
+ */
+
 @Controller
 public class UserController {
     private UserService userService;
@@ -20,22 +25,38 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * When accessing the /home end point will redirect to the home view
+     * @return {String} - returns the home string to view the thymeleaf home page
+     */
     @GetMapping("/home")
     public String homePage() {
         return "home";
     }
 
+    /**
+     * When accessing the /login end point will redirect to the login view
+     * @return {String} - returns the login string to view the thymeleaf login page
+     */
     @GetMapping("/login")
     public String login() {
         return "login";
     }
 
-
+    /**
+     * When accessing the /profile endpoint will redirect to the profile view
+     * @return {String} - returns the profile string to view the thymeleaf profile page
+     */
     @GetMapping("/profile")
     public String profile() {
         return "profile";
     }
 
+    /**
+     * Takes the input from the signup page and stores it in the model object
+     * @param model - the input from the signup page
+     * @return - returns the signup string to view the thymeleaf signup view
+     */
     @GetMapping("/signup")
     public String signupPage(Model model) {
         User newUser = new User();
@@ -43,6 +64,14 @@ public class UserController {
         return "signup";
     }
 
+    /**
+     * Creates a new user checking to make sure the user does not exist and redirects to a success url if
+     * user does not exist and returns back to the signup if user does exist.
+     * @param newUser -
+     * @param result -
+     * @param model -
+     * @return {String} -
+     */
     @PostMapping("/signup/save")
     public String createUser(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model) {
         User existingUser = userService.findByUserEmail(newUser.getEmail());
